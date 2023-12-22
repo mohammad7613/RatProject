f926b789aac0b624d12827bc04e695c936a6803a
MOH-Shafizadegan <mh.shafizadegan@gmail.com>
2023-12-22 14:40:09 +0330

    main code updated

--- codes/NewVersion(mohsh)/main.m
+++ codes/NewVersion(mohsh)/main.m
@@ -1,96 +1,76 @@
-%% Setup and import functions
-
-clc; clear; close all;
+clc; close all; clear;
 
 addpath('Codes/PreProcessing')
 addpath('Codes/Functions')
+addpath('Codes/Visualization')
 
-%% Load data
 
-file_path_to_sessionData = pwd;
-session_no = '1';
-oddball_ratio = '2080';
-exp_no = '2';
-filename = strcat('\sessionDataoddball', session_no, '_', ...
-                   oddball_ratio, '_', exp_no, '.mat');
-file_path = [pwd, '\Data', '\Oddball', filename];
-load(file_path)
-fs = SampleRate;
+%% Data 1:
 
-%%
+clc;
 
-figure;
-plot(digitalByte)
-
+[data, fs] = load_data(pwd, '1', '2080', '2');
 
 %% Preprocess
 clc; close all;
 
-signal = channelData ; 
-tstep = [0.2, 0.9] ; 
+tstep = [0.3, 1] ; 
 band = [1 300] ; 
 rl = [] ;
 type = 'rest&Stimulus'; 
-data = preprocess(tstep, channelData, digitalByte, band, rl, fs, type);
+prep_data = preprocess(tstep, data.channelData, data.digitalByte,...
+                  band, rl, fs, type, 'over_trials');
 
-%% save preprocessed data
+              
+%%
 
-filename = strcat('\sessionDataoddball', session_no, '_', ...
-                   oddball_ratio, '_', exp_no, '_preProc', '.mat');
-file_path = [pwd, '\Data', '\Oddball', filename];
+clc; close all;
 
-save(file_path, 'data')
+directoryPath = strcat('Results/Transfer_Entropy',...
+                       '\sessionDataoddball', '1', '_', ...
+                       '2080', '_', '2'); 
+if exist(directoryPath, 'dir') ~= 7
+        mkdir(directoryPath);
+end
 
+configs = struct(...
+    'lag' , 0.05 * fs,...
+    'interval', 0.4,...
+    'step', 0.1,... 
+    'sig_len', [0.3 1],...
+    'fs', fs...
+    );
 
-%% PAC
+sig_Labels = ["mPFC", "Hippo"];
 
-clc;
-
-thetaBand = [5 8] ; 
-fGamma = [33, 36];
-interval = 0.2 * fs ; 
-step = 0.1 * fs ; 
-
-sig1 = squeeze(mean(data.target(:,:,2),1));
-sig2 = squeeze(mean(data.target(:,:,1),1));
-PAC_target = tfInTrialGram(sig1 ,sig2, fs, interval, step, thetaBand, fGamma, 1);
+[T_target, T_std] = calc_Entropy_inTrial(prep_data, configs, 1, 2);
+plot_Entropy_inTrial(T_std, T_target, sig_Labels, directoryPath, configs)
 
 %%
 
-PAC_mat = PAC_target.table;
-tint = PAC_target.s / fs * 1000 - 200;
-tfGram(PAC_mat,tint,fGamma,1,1)
-title('Target PAC - Driver: Hippo - Reciever: mPFC')
-xline(0, '--')
-set(gca,'CLim',[0 1]);
+clc; close all;
+
+sig_Labels = ["mPFC", "Hippo", "NAcc", "LGN"];
+plot_TEntropy_allConfigs(prep_data, configs, sig_Labels, directoryPath);
+
+%% Enrtopy comodolugram
+
+clc; close all;
+
+[Entropy_table_target, Entropy_table_std] = EntropyComoolugram(prep_data, 2, 1);
+
+%%
+deltaLag = 0.001;
+lags = (0:deltaLag:0.05) * 1000;
+figure; xticks = lags;
+time = -configs.sig_len(1)*1000:configs.step*1000:(configs.sig_len(2)-configs.interval)*1000;
+
+pcolor(lags,time,Entropy_table_target'); shading(gca,'interp');
+colormap(jet);
+set(gca,'FontSize',10);
+xlabel('Lag (ms)','FontSize',10);ylabel('Tine','FontSize',10);
+set(gca,'FontName','Arial');
+set(gca,'XTick',xticks);
+set(gca,'CLim',[0 2]);
 colorbar
-
-%%
-
-tint = PAC_target.s / fs * 1000 - 100;
-figure;
-plot(tint, mean(PAC_mat, 2))
-
-%%
-
-sig1 = squeeze(mean(data.standard(:,:,2),1));
-sig2 = squeeze(mean(data.standard(:,:,1),1));
-PAC_std = tfInTrialGram(sig1 ,sig2, fs, interval, step, thetaBand, fGamma, 1);
-
-%%
-
-PAC_mat = PAC_std.table;
-tint = PAC_std.s / fs * 1000 - 200;
-tfGram(PAC_mat,tint,fGamma,1,1)
-title('Standard PAC - Driver: Hippo - Reciever: mPFC')
-xline(0, '--')
-set(gca,'CLim',[0 1]);
-colorbar
-
-%%
-
-tint = PAC_target.s / fs * 1000 - 200;
-figure;
-plot(tint, mean(PAC_target.table, 2)); hold on;
-plot(tint, mean(PAC_std.table, 2)); hold off;
 
